a2bd37443ab350310f5b859bd6cf46707c3e79c7
lakruone <lakruone@gmail.com>
2024-10-31 17:25:50 +0530

    task two

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -5,5 +5,18 @@ import { redirect } from "next/navigation";
 
 export async function redirectToDashboard(formData: FormData) {
   // CODE FOR TASK 2.4 -----------------------------------------
+  const name: string = formData.get("name") as string;
+
+  const sanitizedName = name
+    .trim()
+    .toLowerCase()
+    .replace(/\s+/g, ' ');
+
+  if (!sanitizedName) {
+    throw new Error("Hey, your name is missing!");
+  }
+
+  cookies().set("gr-name", sanitizedName);
+  redirect("/dashboard");
   // END OF CODE FOR TASK 2.4 ----------------------------------
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,8 +14,40 @@ export default function Page() {
     <main className="bg-teal-100 flex flex-col h-screen">
       <Watermark />
       {error && <Error message={error} />}
-      {/* // CODE FOR TASK 2.2 -----------------------------------------
-      // END OF CODE FOR TASK 2.2 ---------------------------------- */}
+            {/* // CODE FOR TASK 2.2 -----------------------------------------*/}
+            <section className="flex-1 flex flex-col items-center justify-center gap-8 z-10">
+        <div className="animate-pulse text-2xl text-left bg-gradient-to-l from-teal-300 to-teal-600 text-transparent bg-clip-text">
+          Welcome to <br />
+          <span className="text-5xl font-bold">Graph Renderer.</span>
+        </div>
+        
+        <div className="border-2 border-gray-300 rounded p-5 bg-slate-50">
+          <form
+             action={async (formData: FormData) => {
+              try {
+                await redirectToDashboard(formData);
+              } catch (err: any) {
+                setError(err.message);
+              }
+            }}
+          >
+            <label htmlFor="name" className="block text-gray-600 mb-2">
+              What's your name?
+            </label>
+            <div className="flex gap-2 text-gray-700">
+              <Input
+                id="name"
+                type="text"
+                placeholder="e.g. John Appleseed"
+              />
+              <Button type="submit" theme="primary">
+                Continue
+              </Button>
+            </div>
+          </form>
+        </div>
+      </section>
+      {/* // END OF CODE FOR TASK 2.2 ---------------------------------- */}
     </main>
   );
 }
